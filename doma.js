ee1a969828305bb9f0c342ae50a36ec64aa5ecb3
jesusxadrian-hash <jesusxadrian@gmail.com>
2026-05-11 20:39:10 -0500

    Add files via upload

---
+++ doma.js
@@ -0,0 +1,60 @@
+const track = document.getElementById('track');
+const lyricsDisplay = document.getElementById('lyrics');
+const playBtn = document.getElementById('playBtn');
+
+// TIEMPOS RELATIVOS AL CORO (Ajustados al 1:30 que me dijiste)
+const infoCancion = [
+    { seg: 90.00, text: "misterio de mujeeeer" }, 
+    { seg: 93.80, text: "solo será cuestión de tiempo" }, 
+    { seg: 97.10, text: "debo interpretar" },
+    { seg: 99.40, text: "tus gritos, tus llamadas" },
+    { seg: 101.80, text: "tus caricias entrecortadas" },
+    { seg: 105.50, text: "y tus arranques de pasión" },
+    { seg: 110.60, text: "debo buscar dentro" },
+    { seg: 113.50, text: "de tanto desperfecto" },
+    { seg: 115.80, text: "la moraleja de este cuento" },
+    { seg: 118.80, text: "debo domar tu corazón" },
+    { seg: 120.00, text: "Gracias por cantar, bañate por favor"}
+];
+
+playBtn.addEventListener('click', () => {
+    if (track.paused) {
+        // Si es la primera vez que damos play, saltamos al coro (segundo 90)
+        if (track.currentTime < 90) {
+            track.currentTime = 90;
+        }
+        track.play();
+        playBtn.innerText = "PAUSA";
+    } else {
+        track.pause();
+        playBtn.innerText = "REPRODUCIR";
+    }
+});
+
+track.addEventListener('timeupdate', () => {
+    let tiempoActual = track.currentTime;
+
+    // 1. Buscamos la frase
+    let frase = infoCancion.find((item, index) => {
+        let siguiente = infoCancion[index + 1];
+        return tiempoActual >= item.seg && (!siguiente || tiempoActual < siguiente.seg);
+    });
+
+    if (frase) {
+        if (lyricsDisplay.innerText !== frase.text) {
+            lyricsDisplay.style.opacity = 0; 
+            setTimeout(() => {
+                lyricsDisplay.innerText = frase.text;
+                lyricsDisplay.style.opacity = 1;
+            }, 200);
+        }
+    }
+
+    // 2. CORTE AUTOMÁTICO: Si llega al final del coro, se detiene
+    if (tiempoActual >= 130) { // Ajusta este número según dónde quieras que corte
+        track.pause();
+        track.currentTime = 90; // Lo regresa al inicio del coro
+        playBtn.innerText = "REPLAY";
+        
+    }
+});
